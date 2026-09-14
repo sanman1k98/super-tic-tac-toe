29f96c0dedd11b6aa5882ad61be6b10e6b5098a8
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2026-09-14 13:11:55 -0400

    test: use strict assertion mode

--- test/tic-tac-toe.test.ts
+++ test/tic-tac-toe.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable test/no-import-node-test */
 
-import assert from 'node:assert';
+import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import { TicTacToe } from '../src/tic-tac-toe.js';
 
